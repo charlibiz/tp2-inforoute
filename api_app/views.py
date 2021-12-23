4be93ec4580aeeb764726ec3ebbe4e590e51821d
Charles <cbisaillon13@gmail.com>
2021-12-22 21:47:39 -0500

    api reservation

--- api_app/views.py
+++ api_app/views.py
@@ -116,10 +116,14 @@ def room(request, *args, **kwargs):
 @swagger_auto_schema(method="get", tags=["Reservations"])
 @swagger_auto_schema(method="post", tags=["Reservations"], request_body=openapi.Schema(
     type=openapi.TYPE_OBJECT, properties={
-        "town": openapi.Schema(type=openapi.TYPE_STRING, description='Ville dans laquelle se situe la chambre'),
+        "room": openapi.Schema(type=openapi.TYPE_INTEGER, description='ID de la chambre'),
+        "tenant": openapi.Schema(type=openapi.TYPE_INTEGER, description='ID du locataire'),
         "landlord": openapi.Schema(type=openapi.TYPE_INTEGER, description='ID du locateur'),
-        "capacity": openapi.Schema(type=openapi.TYPE_INTEGER, description='Capacité de la chambre'), 
-        "price": openapi.Schema(type=openapi.TYPE_NUMBER, description='Prix quotidien de la chambre'),
+        "nbr_persons": openapi.Schema(type=openapi.TYPE_INTEGER, description='Nombre de personnes dans la réservation'),
+        "in_date": openapi.Schema(type=openapi.FORMAT_DATE, description='Nombre de personnes dans la réservation'),
+        "out_date": openapi.Schema(type=openapi.FORMAT_DATE, description='Nombre de personnes dans la réservation'),
+        "total_price": openapi.Schema(type=openapi.TYPE_NUMBER, description='Prix total'),
+        "satus": openapi.Schema(type=openapi.TYPE_STRING, description='Status de la réservation'),
         }))
 @api_view(["GET", "POST"])
 def reservations(request):
@@ -130,11 +134,14 @@ def reservations(request):
             status=status.HTTP_200_OK,
             )
     if request.method == "POST":
-        town = request.data["town"]
-        capacity = request.data["capacity"]
-        price = request.data["price"]
+        room = request.data["room"]
+        tenant_id = request.data["tenant"]
         landlord_id = request.data["landlord"]
-        reservation = Reservation(town=town, capacity=capacity, price=price, landlord=landlord_id)
+        nbr_persons = request.data["nbr_persons"]
+        in_date = request.data["in_date"]
+        out_date = request.data["out_date"]
+        total_price = request.data["total_price"]
+        reservation = Reservation(room=room, tenant=tenant_id, landlord=landlord_id, nbr_persons=nbr_persons, in_date=in_date, out_date=out_date, total_price=total_price)
         reservation.save()
         reservations_list = Reservation.objects.all()
         return Response(
@@ -146,10 +153,14 @@ def reservations(request):
 @swagger_auto_schema(method="get", tags=["Reservation"], operation_description="GET api/reservation/{id}", manual_parameters=[openapi.Parameter('reservation_id', in_=openapi.IN_PATH, description='ID de la chambre', type=openapi.TYPE_INTEGER)])
 @swagger_auto_schema(method="put", tags=["Reservation"], request_body=openapi.Schema(
     type=openapi.TYPE_OBJECT, properties={
-        "town": openapi.Schema(type=openapi.TYPE_STRING, description='Ville dans laquelle se situe la chambre'),
+        "room": openapi.Schema(type=openapi.TYPE_INTEGER, description='ID de la chambre'),
+        "tenant": openapi.Schema(type=openapi.TYPE_INTEGER, description='ID du locataire'),
         "landlord": openapi.Schema(type=openapi.TYPE_INTEGER, description='ID du locateur'),
-        "capacity": openapi.Schema(type=openapi.TYPE_INTEGER, description='Capacité de la chambre'), 
-        "price": openapi.Schema(type=openapi.TYPE_NUMBER, description='Prix quotidien de la chambre'),
+        "nbr_persons": openapi.Schema(type=openapi.TYPE_INTEGER, description='Nombre de personnes dans la réservation'),
+        "in_date": openapi.Schema(type=openapi.FORMAT_DATE, description='Nombre de personnes dans la réservation'),
+        "out_date": openapi.Schema(type=openapi.FORMAT_DATE, description='Nombre de personnes dans la réservation'),
+        "total_price": openapi.Schema(type=openapi.TYPE_NUMBER, description='Prix total'),
+        "satus": openapi.Schema(type=openapi.TYPE_STRING, description='Status de la réservation'),
         }))
 @swagger_auto_schema(method="delete", tags=["Reservation"], manual_parameters=[openapi.Parameter('reservation_id', in_=openapi.IN_PATH, description='ID de la chambre', type=openapi.TYPE_INTEGER)])
 @api_view(["GET", "PUT", "DELETE"])
@@ -162,17 +173,20 @@ def reservation(request, *args, **kwargs):
             status=status.HTTP_200_OK,
             )
     if request.method == "PUT":
-        town = request.data["town"]
-        capacity = request.data["capacity"]
-        price = request.data["price"]
+        room = request.data["room"]
+        tenant_id = request.data["tenant"]
         landlord_id = request.data["landlord"]
-        reservation = Reservation(id=pk, town=town, capacity=capacity, price=price, landlord=landlord_id)
+        nbr_persons = request.data["nbr_persons"]
+        in_date = request.data["in_date"]
+        out_date = request.data["out_date"]
+        total_price = request.data["total_price"]
+        reservation = Reservation(id=pk, room=room, tenant=tenant_id, landlord=landlord_id, nbr_persons=nbr_persons, in_date=in_date, out_date=out_date, total_price=total_price)
         reservation.save()
         reservations_list = Reservation.objects.all()
         return Response(
-        {"reservations": reservations_list.values()},
-        status=status.HTTP_200_OK,
-        )
+            {"reservations": reservations_list.values()},
+            status=status.HTTP_200_OK,
+            )
     if request.method == "DELETE":
         reservation = Reservation.objects.filter(id=pk).first()
         reservation.delete()
